afcd550db7868b353f76c083edcabef1fbe8242b
anthony6li <anthony6li@163.com>
2018-08-08 09:44:47 +0800

    修改Config/index.js assestPublicPath为  “./” 相对路径。重新Build后提交。

--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
         // Paths
         assetsSubDirectory: 'static',
-        assetsPublicPath: '/',
+        assetsPublicPath: './',
         proxyTable: {
             '/api': {
                 target: 'https://way.jd.com/', // 请换成你的地址
